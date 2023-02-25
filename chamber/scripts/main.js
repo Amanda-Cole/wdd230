b90a4a7cac243d0ccaf1a591aeb77fcbd1b0954d
Amanda-Cole <col22031@byui.edu>
2023-02-25 16:27:44 -0700

    Join page form and table

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -35,6 +35,8 @@ let weekDay = now.getDay();
 if (weekDay !=0 && weekDay <=2){
     document.querySelector(".banner").style.display = "flex";
 }
+// hidden date from Join Form when submit button is clicked
+document.getElementById("hidden-date").innerHTML= new Date();
 
 
 
